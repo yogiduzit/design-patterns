ccfda30be15560fb3f7aaa50fcaaa2be14d9e8f8
herbertma <hma49@sfu.ca>
2020-12-05 15:28:45 -0800

    added simple decorator

---
+++ decorator/Component.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Herbert Ma on 2020-12-05.
+//
+
+
+
+#include "Component.hpp"
+
+WheatDonut::WheatDonut() {
+    name = "Wheat Donut";
+}
+
+RiceDonut::RiceDonut() {
+    name = "Rice Donut";
+}
